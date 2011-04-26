202074721144ee6887d0dde720b30b1742dd1bf7
ishafiul@gmail.com <ishafiul@gmail.com@b5dbf6f8-52c3-e1a3-570e-483b45b40e59>
2011-04-26 18:56:17 +0000

    32

--- trunk/application/models/institution.php
+++ trunk/application/models/institution.php
@@ -5,10 +5,31 @@
  */
  class Institution extends CI_Model {
 
+       var $name ="";
+       var $sname = "";
+       var $campuses = "";
+       var $short_description = "";
+       var $location  = "";
+
        function __construct()
        {
            // Call the Model constructor
            parent::__construct();
+       }
+
+       function Create(){
+           $this->name = $this->input->post('name');
+           $this->sname = $this->input->post('sname');
+           $this->campuses = $this->input->post('campuses');
+           $this->short_description = $this->input->post('short_description');
+           $this->location = $this->input->post('location');
+
+           $this->db->insert('institution',$this);
+           return $this->db->insert_id();
+       }
+
+       function checkAvailability($name, $sname){
+           // given a name & sname checks if they are available
        }
        
        function GetInstitutionList()
