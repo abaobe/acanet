f9d2cf1c83588c24e7f942996ab07530b55985f6
ishafiul <ishafiul@gmail.com>
2011-06-08 19:51:10 +0000

    view latest

--- application/views/default/institution/view.php
+++ application/views/default/institution/view.php
@@ -16,42 +16,51 @@
 
 
 
-                    <h1 class="block">Fields (Dummy)</h1>
+                    <h1 class="block">Fields</h1>
                     <div class="column1-unit">
                         <p><ul>
-                                <li><a href="#">CSE</a></li>
-
-                                <li><a href="#">EEE</a></li>
-                                <li><a href="#">ARCHI</a></li>
-                                <li><a href="#">MME</a></li>
-                                <li><a href="#">CE</a></li>
-                                <li><a href="#">WRE</a></li>
-                                <li><a href="#">NAME</a></li>
-
-                                <li><a href="#">ME</a></li>
+                                <?php
+                                    if(isset($fieldsData)){
+                                        foreach($fieldsData as $row){
+                                            $link = site_url('fields/view') . "/" . $row['field_id'];
+                                            echo "<li><a href = '$link'>" . $row['short_name'] . "</a></li>";
+                                        }
+                                    }
+                                ?>
                             </ul></p>
                     </div>
 
 
-                    <h1 class="block">News (Dummy)</h1>
+                    <h1 class="block">News</h1>
                     <div class="column1-unit">
                         <div class="column1-unit">
-                            <h1>News 1</h1>
-
-                            <h3>Monday, 20 November 2006 at 20:30 GMT, by <a href="#">SiteAdmin </a></h3>
-                            <p>Lorem ipsum dolor sit amet, consectetuer <a href="#">adipiscing elit</a>, sed diam nonummy nibh euismod tincidunt ut laoreet dolore <a href="#">magna aliquam</a> erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis. <a href="#">Read more &raquo;</a></p>
+                            <?php
+                                foreach($newsData as $row){
+                                    $title = anchor('#', $row->heading);
+                                    echo "<h1>" . $title . "</h1>";
+                                    $publisher = anchor('#', $row->publisher_name);
+                                    echo "<h3>Published at " . $row->date_time . " by $publisher_name" . "</h3>";
+                                    echo "<p>" . $row->content . "</p>";
+                                }
+                            ?>
+                            
                         </div>
                         <hr class="clear-contentunit" />
                         <hr class="clear-contentunit" />
 
                     </div>
-                    <h1 class="block">Events (Dummy)</h1>
+                    <h1 class="block">Events</h1>
                     <div class="column1-unit">
                         <div class="column1-unit">
-                            <h1>Event 1</h1>
-                            <h3>Monday, 20 November 2006 at 20:30 GMT, by <a href="#">SiteAdmin </a></h3>
-                            <p>Lorem ipsum dolor sit amet, consectetuer <a href="#">adipiscing elit</a>, sed diam nonummy nibh euismod tincidunt ut laoreet dolore <a href="#">magna aliquam</a> erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis. <a href="#">Read more &raquo;</a></p>
-
+                            <?php
+                                foreach($eventsData as $row){
+                                    $title = anchor('#', $row->title);
+                                    echo "<h1>" . $title . "</h1>";
+                                    $publisher = anchor('#', $row->publisher_name);
+                                    echo "<h3>From " . $row->start_date_time . " to " . $row->end_date_time . " by $publisher" . "</h3>";
+                                    echo "<p>" . $row->description . "</p>";
+                                }
+                            ?>
                         </div>
                         <hr class="clear-contentunit" />
                         <hr class="clear-contentunit" />
